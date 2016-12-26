d0b08e971705d2b20c208c63e02f2bc868442dd0
Kuyawa <haxapp@gmail.com>
2016-12-26 18:32:16 -0400

    Hello world again

--- Sources/App/Controllers/IndexHandler.swift
+++ Sources/App/Controllers/IndexHandler.swift
@@ -5,23 +5,9 @@ import PostgreSQL
 class IndexHandler: WebController {
 
 	func index(_ request: Request) -> ResponseRepresentable {
-		let ini =  0  // get from pagination
-		let max = 50  // get from Config.latestPosts.max
-
-		guard let posts = Posts(in: db).getLatest(start: ini, limit: max) else { 
-			return fail(.databaseUnavailable) 
-		}
-
-		// TODO: Pagination from config max, total rec count, and current page
-		let data: Node = [
-			"forum": ["name": "Latest Messages", "descrip": "From all forums"],
-			"posts": posts,
-			"page" : 0,
-			"pagination":[0,100,200,300,400]
-		]
-
-		let context = getContext(request)
-		let view = getView("index", with: data, in: context)
+		let data: Node = ["forum": ["name": "Latest Messages", "descrip": "From all forums"]]
+		//let context = getContext(request)
+		let view = getView("index", with: data)
 
 		return view!
 	}
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,45 +1,10 @@
 import Vapor
 import HTTP
-import Sessions
-
-let memory = MemorySessions()
-let sessions = SessionsMiddleware(sessions: memory)
 
 let drop = Droplet()
-drop.middleware.append(sessions)
-if drop.environment == .development { print("Sephora is running in dev mode") }
 
-
-// Index
 drop.get(handler: IndexHandler().index)
-drop.get("index", handler: IndexHandler().index)
-
-// Users
-drop.get("register", handler: RegisterHandler().form)
-drop.get("register/user/:user", handler: RegisterHandler().fetchUser)
-drop.get("login", handler: LoginHandler().login)
-drop.get("login/github/:nick", handler: LoginHandler().loginGithub)
-drop.get("authorize", handler: LoginHandler().authorize)
-drop.get("profile", handler: TodoHandler().show)
-drop.get("user", handler: AppHandler().redirectToIndex)
-drop.get("user/:user", handler: UserHandler().show)
-drop.get("logout", handler: LoginHandler().logout)
-
-// Forums
-drop.get("forum") { request in Response(redirect: "/forum/general") }
-drop.get("forum/:forum", handler: ForumHandler().show)
-drop.post("forum/:forum/submit", handler: PostHandler().submit)
-drop.get("forum/:forum/post/:post", handler: PostHandler().show)
-drop.post("forum/:forum/post/:post/reply", handler: ReplyHandler().submit)
-
-// Admin
-drop.get("admin/dbinfo", handler: AdminHandler().dbinfo)
-drop.get("admin/users", handler: AdminHandler().users)
-drop.get("test", handler: TestHandler().show)
-drop.get("404") { request in throw Abort.notFound }
-
 
 drop.run()
-
 
 // End
