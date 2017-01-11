3952f350bb3ef1ec890b40660ff404dcb1adfa44
Kuyawa <haxapp@gmail.com>
2017-01-11 11:16:30 -0400

    Latest posts by date

--- Sources/App/Models/PostsQuery.swift
+++ Sources/App/Models/PostsQuery.swift
@@ -7,24 +7,32 @@ class Posts: DataQuery {
 	// let posts = Posts().getLatest()
 
 	func getLatest(start: Int? = 0, limit: Int? = 30) -> Node? {
-		let sql  = "Select p.*, f.name, f.dirname From posts p, forums f Where f.forumid = p.forumid And p.hidden = false Order by date Desc Offset $1 Limit $2"
+		//OLD: let sql = "Select p.*, f.name, f.dirname From posts p, forums f Where f.forumid = p.forumid And p.hidden = false Order by date Desc Offset $1 Limit $2"
+
+		// Select latest posts or replies order by date desc. Not an easy query but SQL is magic
+		let sub = "Select Distinct on (p.postid) p.postid, p.type, p.title, p.nick, p.date, p.views, p.replies, p.answered, f.name, f.dirname, coalesce(r.date, p.date) as datex From posts p Left Outer Join forums f On p.forumid = f.forumid Left Outer Join replies r On p.postid = r.postid Where p.hidden = false Order by p.postid, datex desc"
+		let sql = "Select * From (\(sub)) latest Order by datex desc Offset $1 Limit $2"
+
 		let args:[Node] = try! [start!.makeNode(), limit!.makeNode()]
-		if let rows = db.query(sql, params: args) {
-			return rows
-		}
-		return nil
+		let rows = db.query(sql, params: args)
+
+		return rows
 	}
 
 	// Get latests posts by forumid
-	// let posts = Posts().getLatest("swift")
+	// let posts = Posts().getLatest(4) //forum/swift
 
 	func getLatest(forumId: Int, start: Int? = 0, limit: Int? = 30) -> Node? {
-		let sql  = "Select p.*, f.name, f.dirname From posts p, forums f Where p.forumid = $1 And f.forumid = p.forumid And p.hidden = false Order by date Desc Offset $2 Limit $3"
+		//OLD: let sql  = "Select p.*, f.name, f.dirname From posts p, forums f Where p.forumid = $1 And f.forumid = p.forumid And p.hidden = false Order by date Desc Offset $2 Limit $3"
+
+		// Select latest posts or replies order by date desc. Not an easy query but SQL is magic
+		let sub = "Select Distinct on (p.postid) p.postid, p.type, p.title, p.nick, p.date, p.views, p.replies, p.answered, f.name, f.dirname, coalesce(r.date, p.date) as datex From posts p Left Outer Join forums f On p.forumid = f.forumid Left Outer Join replies r On p.postid = r.postid Where p.forumid = $1 And p.hidden = false Order by p.postid, datex desc"
+		let sql = "Select * From (\(sub)) latest Order by datex desc Offset $2 Limit $3"
+
 		let args:[Node] = try! [forumId.makeNode(), start!.makeNode(), limit!.makeNode()]
-		if let rows = db.query(sql, params: args) {
-			return rows
-		}
-		return nil
+		let rows = db.query(sql, params: args)
+
+		return rows
 	}
 
 } 
